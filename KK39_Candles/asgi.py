a90749f3ca75214f12addaad4c4b3a38133bf7e7
Kristine <kristine.kamola@gmail.com>
2025-04-15 16:38:45 +0100

    set up Django

---
+++ KK39_Candles/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for KK39_Candles project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'KK39_Candles.settings')
+
+application = get_asgi_application()
