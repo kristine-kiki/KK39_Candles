6b27b1accfa0892d758c70159485070b8f36690f
Kristine <kristine.kamola@gmail.com>
2025-04-26 11:44:09 +0100

    updated data base

--- products/models.py
+++ products/models.py
@@ -26,6 +26,7 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    is_bestseller = models.BooleanField(default=False, help_text="Display this on the homepage bestseller section?")
 
     def __str__(self):
         return self.name
