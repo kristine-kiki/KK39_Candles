723013982ac3ff656ab88fa5be641bfda468be7e
Kristine <kristine.kamola@gmail.com>
2025-05-09 13:05:16 +0100

    add product view,url and template in Product Admin

--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models.functions import Lower
 from django.db.models import Q
 from .models import Product, Category
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -76,10 +78,9 @@ def product_detail(request, product_id):
     }
     return render(request, 'products/products_detail.html', context)
 
-"""
 @login_required
 def add_product(request):
-     Add a product to the store
+    
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
@@ -99,4 +100,22 @@ def add_product(request):
     context = {'form': form}
 
     return render(request, template, context)
-"""
+
+@login_required
+def edit_product(request, product_id):
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect('home')
+
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Successfully updated product: {product.name}!')
+            return redirect('product_detail', product_id=product.id) # Or redirect
+        else:
+            messages.error(request, f'Failed to update product. Please correct the errors below.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing "{product.name}"')
--- profiles/views.py
+++ profiles/views.py
@@ -91,3 +91,4 @@ def order_history(request, order_number):
     }
 
     return render(request, template, context)
+
