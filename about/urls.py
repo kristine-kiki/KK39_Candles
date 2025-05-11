ba12ed8b850a0f40760c2159a869b441540539c9
Kristine <kristine.kamola@gmail.com>
2025-05-11 17:47:38 +0100

    add about links to base.html,checked funcionality

--- about/urls.py
+++ about/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('our-story/', views.our_story_view, name='our_story'),
+    path('our_story/', views.our_story_view, name='our_story'),
     path('ingredients/', views.ingredients_view, name='ingredients'),
 ]
