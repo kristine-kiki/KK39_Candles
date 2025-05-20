af25426ccdcf8dcf840d9019105bc0bbfa667ad4
Kristine <kristine.kamola@gmail.com>
2025-05-20 15:24:58 +0100

    delete runtime.txt and created .python-version as per heroku advise

--- KK39_Candles/settings.py
+++ KK39_Candles/settings.py
@@ -28,12 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ 
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
-    'kk39-candles-24591ccdc142.herokuapp.com',
+    'kk39-candles-24591ccdc142.herokuapp.com'
 ]
 
 
