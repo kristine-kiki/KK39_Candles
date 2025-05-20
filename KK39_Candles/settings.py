b8c7da861aa1236be2240c2df2ceeed84614c8ae
Kristine <kristine.kamola@gmail.com>
2025-05-20 20:57:36 +0100

    update allauth templates

--- KK39_Candles/settings.py
+++ KK39_Candles/settings.py
@@ -222,6 +222,7 @@ STRIPE_MINIMUM_AMOUNT_CENTS = 30
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET','')
+
 DEFAULT_FROM_EMAIL = 'hello@kk39candles.com'
 CONTACT_EMAIL = 'hello@kk39candles.com'
 
